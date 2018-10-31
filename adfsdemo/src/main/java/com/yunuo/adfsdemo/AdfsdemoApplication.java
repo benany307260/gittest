82be3b5ad699c2cdac100b67757c385aeedda93c
benany307260 <307260621@qq.com>
2018-10-31 15:37:51 +0800

    创建adfs测试web

---
+++ adfsdemo/src/main/java/com/yunuo/adfsdemo/AdfsdemoApplication.java
@@ -0,0 +1,12 @@
+package com.yunuo.adfsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdfsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdfsdemoApplication.class, args);
+	}
+}
